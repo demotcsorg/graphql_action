4cbd00b1a4e38e34c5e8b3e7a6bed405a8284acc
NisargShah1410 <nisargshah01410@gmail.com>
2022-03-10 15:41:07 +0530

    seventh

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ else if(OPERATION == "query_commit"){
     .then((txt) => {
       const data = JSON.parse(txt);
       var cropped = { data: [] };
-      cropped["data"] = data["data"]["user"]["repository"]["nodes"];
+      cropped["data"] = data["data"]["user"]["repository"]["edges"];
       // const orgs = data["data"]["user"]["repository"]["nodes"];
       // var newOrgs = { data: [] };
   
