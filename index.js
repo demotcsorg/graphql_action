6f86ce6be8cfb3f68cb03056643ec6f8707f32da
NisargShah1410 <nisargshah01410@gmail.com>
2022-03-10 14:03:35 +0530

    third

--- index.js
+++ index.js
@@ -11,75 +11,50 @@ console.log(`Github Token ${GITHUB_TOKEN}`);
 console.log(`Username ${USERNAME}`);
 console.log(`Operation ${OPERATION}`);
 
-const query_issue = {
-    query: `query{
-          user(login: "${USERNAME}") {
-      issues(last: 100, orderBy: {field:CREATED_AT, direction: DESC}){
-        totalCount
-        nodes{
-            id
-          closed
-          title
-          createdAt
-          url
-          number
-          assignees(first:100){
-            nodes{
-              avatarUrl
-              name
-              url
-            }
-          }
-          repository{
-            name
-            url
-            owner{
-              login
-              avatarUrl
-              url
-            }
-          }
-        }
-      }
-    }
-}`,
-};
-const query_pr = {
-  query: `
-	query {
-	  user(login: "${USERNAME}"){
-	    pullRequests(last: 100, orderBy: {field: CREATED_AT, direction: DESC}){
-      totalCount
-      nodes{
-        id
-        title
-        url
-        state
-	      mergedBy {
-	          avatarUrl
-	          url
+const query_org = {
+  query: `query{
+	user(login: "${openSource.githubUserName}") {
+	    repositoriesContributedTo(last: 100){
+	      totalCount
+	      nodes{
+	        owner{
 	          login
-	      }
-	      createdAt
-	      number
-        changedFiles
-	      additions
-	      deletions
-        baseRepository {
-	          name
-	          url
-	          owner {
-	            avatarUrl
-	            login
-	            url
-	          }
+	          avatarUrl
+	          __typename
 	        }
-      }
-    }
+	      }
+	    }
+	  }
+	}`,
+};
+
+const query_pinned_projects = {
+  query: `
+	query { 
+	  user(login: "${openSource.githubUserName}") { 
+	    pinnedItems(first: 6, types: REPOSITORY) {
+	      totalCount
+	      nodes{
+	        ... on Repository{
+	          id
+		          name
+		          createdAt,
+		          url,
+		          description,
+		          isFork,
+		          languages(first:10){
+		            nodes{
+		              name
+		            }
+		          }
+	        }
+	      }
+		  }
+	  }
 	}
-}
 	`,
 };
+
 const query_commit = {
   query: `
   query {
@@ -116,68 +91,83 @@ const headers = {
 console.log('before call');
 
 
-  if(OPERATION == "query_issues"){
+  if(OPERATION == "query_org"){
 fetch(baseUrl, {
     method: "POST",
     headers: headers,
-    body: JSON.stringify(query_issue),
+    body: JSON.stringify(query_org),
   })
     .then((response) => response.text())
     .then((txt) => {
-      const data = JSON.parse(txt);
-      //console.log('Data Obj');
-      //console.log(JSON.stringify(data["data"]["user"]["issues"]));
-      var cropped = { data: [] };
-      cropped["data"] = data["data"]["user"]["issues"]["nodes"];
-  
-      var open = 0;
-      var closed = 0;
-      for (var i = 0; i < cropped["data"].length; i++) {
-        if (cropped["data"][i]["closed"] === false) open++;
-        else closed++;
-      }
-  
-      cropped["open"] = open;
-      cropped["closed"] = closed;
-      cropped["totalCount"] = cropped["data"].length;
-  
-      console.log("Fetching the Issues Data.\n");
+        const data = JSON.parse(txt);
+        const orgs = data["data"]["user"]["repositoriesContributedTo"]["nodes"];
+        var newOrgs = { data: [] };
+        for (var i = 0; i < orgs.length; i++) {
+          var obj = orgs[i]["owner"];
+          if (obj["__typename"] === "Organization") {
+            var flag = 0;
+            for (var j = 0; j < newOrgs["data"].length; j++) {
+              if (JSON.stringify(obj) === JSON.stringify(newOrgs["data"][j])) {
+                flag = 1;
+                break;
+              }
+            }
+            if (flag === 0) {
+              newOrgs["data"].push(obj);
+            }
+          }
+        }
+
+      console.log("Fetching the Contributed Organization Data.\n");
       console.log(JSON.stringify(cropped))
 
 
       
     })
     .catch((error) => console.log(JSON.stringify(error)))
-
-
 }
-else if(OPERATION == "query_pr"){
+
+
+else if(OPERATION == "query_pinned_projects"){
+
+  const languages_icons = {
+    Python: "logos-python",
+    "Jupyter Notebook": "logos-jupyter",
+    HTML: "logos-html-5",
+    CSS: "logos-css-3",
+    JavaScript: "logos-javascript",
+    "C#": "logos-c-sharp",
+    Java: "logos-java",
+  };
+
   fetch(baseUrl, {
     method: "POST",
     headers: headers,
-    body: JSON.stringify(query_pr),
+    body: JSON.stringify(query_pinned_projects),
   })
     .then((response) => response.text())
     .then((txt) => {
       const data = JSON.parse(txt);
-      var cropped = { data: [] };
-      cropped["data"] = data["data"]["user"]["pullRequests"]["nodes"];
-  
-      var open = 0;
-      var closed = 0;
-      var merged = 0;
-      for (var i = 0; i < cropped["data"].length; i++) {
-        if (cropped["data"][i]["state"] === "OPEN") open++;
-        else if (cropped["data"][i]["state"] === "MERGED") merged++;
-        else closed++;
+      // console.log(txt);
+      const projects = data["data"]["user"]["pinnedItems"]["nodes"];
+      var newProjects = { data: [] };
+      for (var i = 0; i < projects.length; i++) {
+        var obj = projects[i];
+        var langobjs = obj["languages"]["nodes"];
+        var newLangobjs = [];
+        for (var j = 0; j < langobjs.length; j++) {
+          if (langobjs[j]["name"] in languages_icons) {
+            newLangobjs.push({
+              name: langobjs[j]["name"],
+              iconifyClass: languages_icons[langobjs[j]["name"]],
+            });
+          }
+        }
+        obj["languages"] = newLangobjs;
+        newProjects["data"].push(obj);
       }
-  
-      cropped["open"] = open;
-      cropped["closed"] = closed;
-      cropped["merged"] = merged;
-      cropped["totalCount"] = cropped["data"].length;
-  
-      console.log("Fetching the Pull Request Data.\n");
+
+      console.log("Fetching the Pinned Projects Data.\n");
       console.log(JSON.stringify(cropped))
     })
     .catch((error) => console.log(JSON.stringify(error)));
